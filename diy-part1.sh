e779e659dafaf35ccc7d044345ab85098998c099
wxl5343329 <5343329@qq.com>
2026-06-12 21:44:44 +0800

    Add passwall and passwall2 sources

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,9 +10,8 @@
 # See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 添加科学上网插件源
+echo src-git helloworld https://github.com/fw876/helloworld >>feeds.conf.default
+echo src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main >>feeds.conf.default
+echo src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main >>feeds.conf.default
+echo src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main >>feeds.conf.default
